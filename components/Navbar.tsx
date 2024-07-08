7f5bb447544496754cc2387fba2bea443de4766f
Dhruva Kumar <102192960+subbuguru@users.noreply.github.com>
2024-07-08 14:54:06 -0500

    Updates to home page
    updates to navbar and hero section. Plan right now is to create a bento grid and a contact form on the main page.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-900 text-white p-4">
+    <nav className=" text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
         <div>
           <h1 className="text-xl font-bold">Dhruva Kumar</h1>
