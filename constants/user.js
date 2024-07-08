4bc579841bb5feb80ebf65ee73673b39ee5b2e30
Dhruva Kumar <102192960+subbuguru@users.noreply.github.com>
2024-07-08 18:50:22 -0500

    Create user.js
    Began updating my user data

---
+++ constants/user.js
@@ -0,0 +1,54 @@
+const userData = {
+    githubUsername: "manuarora700",
+    name: "Manu Arora",
+    designation: "Full-Stack Developer",
+    avatarUrl: "/avatar.png",
+    email: "manuarorawork@gmail.com",
+    phone: "+91 9587738861",
+    address: "Jaipur, Rajasthan, India.",
+    projects: [
+      
+      {
+        title: "My Meds - Personal Meds List",
+        link: "https://devmedium.vercel.app",
+        imgUrl: "/devmedium.png",
+      },
+    ],
+    about: {
+      title:
+        "",
+      description: [
+        ""
+      ],
+      //currentProject: "Tailwind Master Kit",
+      //currentProjectUrl: "https://tailwindmasterkit.com",
+    },
+    experience: [
+    
+      {
+        title: "Intern",
+        company: "Sagiss",
+        year: "2024",
+        companyLink: "sagiss.com",
+        desc: "Took client calls and created team reports using SQL and Power BI",
+      },
+      {
+        title: "",
+        company: "Mathnasium KV",
+        year: "2023-2024",
+        companyLink: "mathnasium.com",
+        desc: "Instructed up 4 students per hour on Math topics from Grades 1-10",
+      },
+      
+    ],
+    resumeUrl:
+      "https://drive.google.com/file/d/1EQA6AfxprOjX08u7L17LW-p9gz6cJFA9/view?usp=sharing",
+    socialLinks: {
+
+      linkedin: "https://linkedin.com/in/manuarora28",
+      github: "https://github.com/manuarora700",
+
+    },
+  };
+  
+  export default userData;
