7ba386f35136ef992a2563612adc428f20ce2496
Dhruva Kumar <102192960+subbuguru@users.noreply.github.com>
2024-07-09 10:55:28 -0500

    Fixed responsiveness on home page
    Fixed an issue where text was invisible in cards because overflow was hidden

--- components/homepage/HomeCard.tsx
+++ components/homepage/HomeCard.tsx
@@ -15,7 +15,7 @@ const HomeCard: React.FC<HomeCardProps> = ({ title, description, buttonText, hre
     <Card className="min-h-[200px] flex flex-col justify-between">
       <CardHeader>
         <CardTitle className="text-xl">{title}</CardTitle>
-        <CardDescription className="text-sm h-12 overflow-hidden">{description}</CardDescription>
+        <CardDescription className="text-sm h-12">{description}</CardDescription>
       </CardHeader>
       <CardContent className="mt-auto">
         <Link href={href} passHref>
