d8a8e7d71eb2ebd30d50d250a11cbe251c8ebc8e
Dhruva Kumar <102192960+subbuguru@users.noreply.github.com>
2024-07-09 10:35:24 -0500

    Completed first draft of experience page
    Copied a lot- try to do so less next time

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,7 +4,7 @@ import userData from '@constants/user'
 
 const Navbar = () => {
   return (
-    <nav className="p-4 border-solid border-b-2 border-muted">
+    <nav className="p-4 border-solid border-b-2 border-muted ">
       <div className="container  mx-auto flex items-center justify-between ">
         <div className="w-1/4">
           <Link className='block text-left' href="/">
@@ -18,6 +18,7 @@ const Navbar = () => {
             <Link href="/wildlife" className="hover:text-gray-300">Wildlife</Link>
             <Link href="/experience" className="hover:text-gray-300">Experience</Link>
             <Link href="/projects" className="hover:text-gray-300">Projects</Link>
+            <Link href="/about" className='hover:text-gray-300'>About</Link>
           </div>
         </div>
         <div className="w-1/4 flex justify-end">
---
+++ components/experience/ExperienceCard.tsx
@@ -0,0 +1,26 @@
+interface ExperienceCardProps {
+    title: string;
+    desc: string;
+    year: string;
+    company: string;
+    companyLink: string;
+  }
+  
+  export
+
+
+const ExperienceCard: React.FC<ExperienceCardProps> = ({ title, desc, year, company, companyLink }) => {
+    return (
+        
+      <div className="relative border bg-white dark:bg-black p-4 border-muted rounded-md shadow-xl  z-10 mx-4 max-w-[500px] justify-center">
+        <h1 className="absolute -top-10 md:-left-5 md:-top-10 text-4xl text-gray-500 font-extrabold">
+          {year}
+        </h1>
+        <h1 className="font-semibold text-xl">{title}</h1>
+        <a href={companyLink} className="text-gray-500">
+          {company}
+        </a>
+        <p className="text-gray-400 my-2">{desc}</p>
+      </div>
+    );
+  };
