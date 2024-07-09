24f4340293870a28eb7c5636703e506c0cbb6361
Dhruva Kumar <102192960+subbuguru@users.noreply.github.com>
2024-07-08 19:08:08 -0500

    Completed responsive header
    Finished Responsive header

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,25 +1,25 @@
 import Link from 'next/link'
+import { ModeToggle } from './ui/mode-toggle'
 
 const Navbar = () => {
   return (
-    <nav className=" text-white p-4">
+    <nav className=" p-4">
       <div className="container mx-auto flex justify-between items-center">
         <div>
-          <h1 className="text-xl font-bold">Dhruva Kumar</h1>
-          <p className="text-sm text-gray-400">Developer & Photographer</p>
+        <Link href="/">
+            <h1 className="text-xl font-bold">Dhruva Kumar</h1>
+            <p className="text-sm text-gray-400">Developer & Photographer</p>
+        </Link>
         </div>
-        <div className="flex items-center space-x-6">
-          <Link href="/about" className="hover:text-gray-300">About</Link>
-          <Link href="/projects" className="hover:text-gray-300">Projects</Link>
+        <div className="flex items-center space-x-6 invisible md:visible">
+          <Link href="/real-estate" className="hover:text-gray-300">Real Estate</Link>
+          <Link href="/wildlife" className="hover:text-gray-300">Wildlife</Link>
           <Link href="/experience" className="hover:text-gray-300">Experience</Link>
-          <Link href="/contact" className="hover:text-gray-300">Contact</Link>
-          <div className="flex space-x-4">
-            <a href="#" className="hover:text-gray-300"><i className="fab fa-instagram"></i></a>
-            <a href="#" className="hover:text-gray-300"><i className="fab fa-twitter"></i></a>
-            <a href="#" className="hover:text-gray-300"><i className="fab fa-linkedin"></i></a>
-            
-          </div>
+          <Link href="/projects" className="hover:text-gray-300">Projects</Link>
+          
+          
         </div>
+        <ModeToggle />
       </div>
     </nav>
   )
