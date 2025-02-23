f3863187ab46907ec6adb1990112b703d992b2bb
Dhruva Kumar <unkumar28@gmail.com>
2025-02-23 17:10:57 -0600

    fix alignment of project card by removing justify between and adding flex grow

--- components/BentoGrid.tsx
+++ components/BentoGrid.tsx
@@ -42,12 +42,11 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "row-span-1 rounded-md group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
+        "row-span-1 rounded-md group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent flex flex-col space-y-4",
         className
       )}
     >
-      <div className="max-w-[100%] relative w-full aspect-[16/9] overflow-hidden"> {/*Set a fixed max width / height in div, then you can set whatever img size you want in next js it will only go to fill its container
-                                                            */}
+      <div className="max-w-[100%] relative w-full aspect-[16/9] overflow-hidden"> 
         <Link href={`${link}`} rel="noopener noreferrer" target="_blank">
             <Image
                 src={`${imgUrl}`}
@@ -58,17 +57,19 @@ export const BentoGridItem = ({
             />
         </Link>
       </div>
-      <div className="group-hover/bento:translate-x-2 transition duration-200">
-        <div className="font-sans font-bold text-xl mb-2 mt-2">
-          {title}
+      <div className="flex flex-col flex-grow">
+        <div className="group-hover/bento:translate-x-2 transition duration-200">
+          <div className="font-sans font-bold text-xl mb-2 mt-2">
+            {title}
+          </div>
+          <div className="font-sans font-normal text-neutral-600 dark:text-neutral-300 mb-4">
+            {description}
+          </div>
         </div>
-        <div className="font-sans font-normal text-neutral-600 dark:text-neutral-300 mb-4">
-          {description}
-        </div>
-        <div className="font-sans font-semibold">
+        
+        <div className="mt-auto font-sans font-semibold">
           {technologies}
         </div>
-
       </div>
     </div>
   );
