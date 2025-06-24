1579ac22e8c728ed778c0923edfbb238503bee3b
Dhruva Kumar <102192960+subbuguru@users.noreply.github.com>
2025-06-24 10:52:40 -0500

    homepage container refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,18 @@ import userData from "@/constants/user";
 
 export default function Home() {
   return (
-    <main>
-      <section className="w-full py-12 md:py-24">
-        <div className="container px-4 md:px-8 text-center flex-col">
-          <div className="flex flex-wrap justify-center items-center text-center py-4">
-            <h1 className="mr-2 text-5xl font-bold tracking-tighter sm:text-6xl md:text-8xl">
-              The Intersection of Code and Capture.
-            </h1>
-          </div>
-          <p className="mx-auto text-muted-foreground text-xl md:text-2xl">
-            {userData.home.heroText}
-          </p>
+    <main className="container">
+      <section className="w-full py-12 md:py-24 px-4 md:px-8 text-center flex-col">
+        <div className="flex flex-wrap justify-center items-center text-center py-4">
+          <h1 className="mr-2 text-5xl font-bold tracking-tighter sm:text-6xl md:text-8xl">
+            The Intersection of Code and Capture.
+          </h1>
         </div>
+        <p className="mx-auto text-muted-foreground text-xl md:text-2xl">
+          {userData.home.heroText}
+        </p>
       </section>
-      <div className="container mx-auto text-foreground p-12">
+      <div className="text-foreground p-12">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           <HomeCard
             title="Experience"
